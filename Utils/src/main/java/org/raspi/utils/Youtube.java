7b7301a125d76541770daf360d0578d166fa75ac
Vignesh Arjunan <vignesh.arjunan@gmail.com>
2016-01-27 23:08:26 +0530

    created Parent POM and improved download experience

--- Utils/src/main/java/org/raspi/utils/Youtube.java
+++ Utils/src/main/java/org/raspi/utils/Youtube.java
@@ -5,6 +5,7 @@
  */
 package org.raspi.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +30,8 @@ public class Youtube {
         }
         return ifconfigFlags.getInputMsg();
     }
-    
-     public void killAllYoutubeDl() throws IOException {
+
+    public void killAllYoutubeDl() throws IOException {
         // killing already running players 
         List<String> commands = new ArrayList<>();
         commands.add("/usr/bin/killall");
@@ -38,9 +39,9 @@ public class Youtube {
         commands.add("youtube-dl");
         ProcessExecutor killer = new ProcessExecutor(commands);
         killer.startExecutionNonBlocking();
-        
+
         commands.clear();
-        commands.add("/bin/rm");    
+        commands.add("/bin/rm");
         commands.add("-f");
         commands.add("/home/pi/*.part");
         ProcessExecutor partFileRemoves = new ProcessExecutor(commands);
@@ -48,8 +49,14 @@ public class Youtube {
     }
 
     private ProcessExecutor processExecutor;
+    private final String youtubeURL;
+    private File file;
 
-    public String download(String youtubeURL) throws IOException {
+    public Youtube(String youtubeURL) {
+        this.youtubeURL = youtubeURL;
+    }
+
+    public String download() throws IOException {
         List<String> commands = new ArrayList<>();
         commands.add("/usr/local/bin/youtube-dl");
         commands.add(youtubeURL);
@@ -60,6 +67,25 @@ public class Youtube {
             throw new IllegalArgumentException(ifconfigFlags.getErrMsg());
         }
         return ifconfigFlags.getInputMsg();
+    }
+
+    public File getFile() throws IOException {
+
+        if (file != null) {
+            return file;
+        }
+
+        List<String> commands = new ArrayList<>();
+        commands.add("/usr/local/bin/youtube-dl");
+        commands.add("--get-filename");
+        commands.add(youtubeURL);
+        ProcessExecutor getFileProcessExecutor = new ProcessExecutor(commands);
+        Flags ifconfigFlags = getFileProcessExecutor.startExecution();
+        if (ifconfigFlags.getErrMsg().length() > 0) {
+            killAllYoutubeDl();
+            throw new IllegalArgumentException(ifconfigFlags.getErrMsg());
+        }
+        return file = new File(ifconfigFlags.getInputMsg());
     }
 
     public ProcessExecutor getProcessExecutor() {
